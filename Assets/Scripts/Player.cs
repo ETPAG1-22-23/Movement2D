27efec10cb862f3f1dc690e1c6cccfbae6a6d909
GitHubLeQuellec <le.quellec.kilian@gmail.com>
2023-01-25 17:00:12 +0100

    prémisse crouching player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     SpriteRenderer sr;
     Animator animController;
     float horizontal_value;
+    float vertical_value;
     Vector2 ref_velocity = Vector2.zero;
 
     float jumpForce = 12f;
@@ -15,6 +16,8 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed_horizontal = 400.0f;
     [SerializeField] bool is_jumping = false;
     [SerializeField] bool can_jump = false;
+    [SerializeField] bool is_crouching = false;
+    [SerializeField] bool can_crouch = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
 
 
@@ -22,6 +25,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CapsuleCollider2D>();
         sr = GetComponent<SpriteRenderer>();
         animController = GetComponent<Animator>();
         //Debug.Log(Mathf.Lerp(current, target, 0));
@@ -42,6 +46,17 @@ public class Player : MonoBehaviour
             is_jumping = true;
             animController.SetBool("Jumping", true);
         }
+
+        if (Input.GetButtonDown("Vertical") && can_crouch) // Definie l'etat du player comme accroupi et change l'animation
+        {
+            is_crouching = true;
+            animController.SetBool("Crouching", true);
+        }
+        if (Input.GetButtonUp("Vertical") && can_crouch) // Definie l'etat du player comme pas accroupi et change l'animation
+        {
+            is_crouching = false;
+            animController.SetBool("Crouching", false);
+        }
     }
     void FixedUpdate()
     {
@@ -50,7 +65,14 @@ public class Player : MonoBehaviour
             is_jumping = false;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             can_jump = false;
+            can_crouch = false; // ne peut plus crouch (car en train de sauter)
         }
+
+        if(is_crouching && can_crouch)
+        {
+            Debug.Log("accroupi baby"); // test accroupi ou non
+        }
+
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
         
@@ -59,6 +81,7 @@ public class Player : MonoBehaviour
     {
         can_jump = true;
         animController.SetBool("Jumping", false);
+        can_crouch = true; // peut s'accroupir car au sol
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {   
