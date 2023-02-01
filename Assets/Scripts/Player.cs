4956a31ee440b6846421f140dcb985cc6a88903d
GitHubLeQuellec <le.quellec.kilian@gmail.com>
2023-02-01 13:38:49 +0100

    debut crouch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,19 +12,12 @@ public class Player : MonoBehaviour
 
     float jumpForce = 12f;
 
-    bool canDash = true;
-    bool isDashing;
-    float dashingPower = 100f;
-    float dashingTime = 0.2f;
-    float dashingCooldown = 0.5f;
-
-
     [SerializeField] TrailRenderer tr;
     [SerializeField] float moveSpeed_horizontal = 400.0f;
     [SerializeField] bool is_jumping = false;
-    [SerializeField] bool can_jump = false;
+    [SerializeField] bool grounded = false;
+    [SerializeField] bool is_crouching = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
-
 
     // Start is called before the first frame update
     void Start()
@@ -38,11 +31,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isDashing)
-        {
-            return;
-        }
-
         horizontal_value = Input.GetAxis("Horizontal");
 
         if(horizontal_value > 0) sr.flipX = false;
@@ -50,66 +38,48 @@ public class Player : MonoBehaviour
         
         animController.SetFloat("Speed", Mathf.Abs(horizontal_value));
    
-        if (Input.GetButtonDown("Jump") && can_jump)
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             is_jumping = true;
             animController.SetBool("Jumping", true);
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (Input.GetButton("Vertical") && grounded)
         {
-            StartCoroutine(Dash());
+            is_crouching = true;
+            // anim
         }
+        else
+        {
+            is_crouching = false;
+        }
+
     }
     void FixedUpdate()
     {
-        if (is_jumping && can_jump)
+        if (is_jumping && grounded)
         {           
             is_jumping = false;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            can_jump = false;
+            grounded = false;
+        }
+        if (is_crouching && grounded)
+        {
+            //rigidbody
+            Debug.Log("je crouch !");
         }
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
 
-        if (isDashing)
-        {
-            return;
-        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        can_jump = true;
+        grounded = true;
         animController.SetBool("Jumping", false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {   
         animController.SetBool("Jumping", false);        
     }
-
-
-    //CODE DASH
-    private IEnumerator Dash()
-    {
-         canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        if (sr.flipX == true)
-        {
-            rb.velocity = new Vector2(-transform.localScale.x * dashingPower, 0f);
-        }
-        else
-        { rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f); }
-
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
-
 
 }
