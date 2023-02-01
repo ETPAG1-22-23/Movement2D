1525c505fcb842bbd72aa291c1ac58a49c3c798f
GitHubLeQuellec <le.quellec.kilian@gmail.com>
2023-02-01 17:14:31 +0100

    crouch_fini

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    CapsuleCollider2D cap;
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator animController;
@@ -19,9 +21,14 @@ public class Player : MonoBehaviour
     [SerializeField] bool is_crouching = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
 
+    bool CheckSphere;
+    private Vector2 aidepose;
+    [SerializeField] GameObject aide;
+
     // Start is called before the first frame update
     void Start()
     {
+        cap = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animController = GetComponent<Animator>();
@@ -31,6 +38,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         horizontal_value = Input.GetAxis("Horizontal");
 
         if(horizontal_value > 0) sr.flipX = false;
@@ -43,17 +51,6 @@ public class Player : MonoBehaviour
             is_jumping = true;
             animController.SetBool("Jumping", true);
         }
-
-        if (Input.GetButton("Vertical") && grounded)
-        {
-            is_crouching = true;
-            // anim
-        }
-        else
-        {
-            is_crouching = false;
-        }
-
     }
     void FixedUpdate()
     {
@@ -63,20 +60,51 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             grounded = false;
         }
-        if (is_crouching && grounded)
+
+        if (Input.GetButton("Vertical") && grounded);
         {
-            //rigidbody
-            Debug.Log("je crouch !");
+            Crouch();
         }
-        Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
+            
+
+        Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.deltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
 
+    }
+    private void Crouch()
+    {
+        if (Input.GetButton("Vertical") && grounded)
+        {
+            aidepose = new Vector2(aide.transform.position.x, aide.transform.position.y);
+            CheckSphere = Physics2D.OverlapCircle(aidepose, 0.1f);
+            is_crouching = true;
+            moveSpeed_horizontal = 200f;
+            cap.offset = new Vector2(0.1f, -0.6f);
+            cap.size = new Vector2(1.1f, 0.8f);
+            cap.direction = CapsuleDirection2D.Horizontal;
+            animController.SetBool("Crouching", true);
+        }
+        else
+        {
+            aidepose = new Vector2(aide.transform.position.x, aide.transform.position.y);
+            CheckSphere = Physics2D.OverlapCircle(aidepose, 0.1f);
+            if (CheckSphere == false)
+            {
+                is_crouching = false;
+                moveSpeed_horizontal = 400f;
+                cap.offset = new Vector2(0f, -0.35f);
+                cap.size = new Vector2(1f, 1.3f);
+                cap.direction = CapsuleDirection2D.Vertical;
+                animController.SetBool("Crouching", false);
+            }
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         grounded = true;
         animController.SetBool("Jumping", false);
     }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {   
         animController.SetBool("Jumping", false);        
