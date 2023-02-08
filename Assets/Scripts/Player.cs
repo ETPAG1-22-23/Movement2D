3ac1e93f96e7d9325ac8758ff37b70102348944c
GitHubLeQuellec <le.quellec.kilian@gmail.com>
2023-02-08 17:03:44 +0100

    climbing_sans_sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     CapsuleCollider2D cap;
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator animController;
     float horizontal_value;
+    float vertical_value;
     Vector2 ref_velocity = Vector2.zero;
 
     float jumpForce = 12f;
@@ -20,6 +20,9 @@ public class Player : MonoBehaviour
     [SerializeField] bool grounded = false;
     [SerializeField] bool is_crouching = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
+    [SerializeField] float Climb_speed = 100f;
+    public bool isLadder = false;
+    public bool canClimb = false;
 
     bool CheckSphere;
     private Vector2 aidepose;
@@ -38,26 +41,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         horizontal_value = Input.GetAxis("Horizontal");
+        vertical_value = Input.GetAxis("Vertical");
 
-        if(horizontal_value > 0) sr.flipX = false;
+        if (horizontal_value > 0) sr.flipX = false;
         else if (horizontal_value < 0) sr.flipX = true;
         
         animController.SetFloat("Speed", Mathf.Abs(horizontal_value));
    
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Jump") && grounded && !is_crouching)
         {
             is_jumping = true;
-            animController.SetBool("Jumping", true);
         }
     }
     void FixedUpdate()
     {
-        if (is_jumping && grounded)
+        if (is_jumping && grounded && !canClimb)
         {           
             is_jumping = false;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            animController.SetBool("Jumping", true);
             grounded = false;
         }
 
@@ -65,18 +68,26 @@ public class Player : MonoBehaviour
         {
             Crouch();
         }
-            
 
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.deltaTime, rb.velocity.y);
+
+        if (canClimb && vertical_value != 0)
+        {
+            rb.gravityScale = 0f;
+            grounded = false;
+            target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.deltaTime, vertical_value * Climb_speed * Time.deltaTime);
+            Debug.Log("Test");
+        }
+        
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
 
     }
     private void Crouch()
     {
-        if (Input.GetButton("Vertical") && grounded)
+        aidepose = new Vector2(aide.transform.position.x, aide.transform.position.y);
+        CheckSphere = Physics2D.OverlapCircle(aidepose, 0.1f);
+        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && grounded)
         {
-            aidepose = new Vector2(aide.transform.position.x, aide.transform.position.y);
-            CheckSphere = Physics2D.OverlapCircle(aidepose, 0.1f);
             is_crouching = true;
             moveSpeed_horizontal = 200f;
             cap.offset = new Vector2(0.1f, -0.6f);
@@ -84,30 +95,43 @@ public class Player : MonoBehaviour
             cap.direction = CapsuleDirection2D.Horizontal;
             animController.SetBool("Crouching", true);
         }
-        else
+        else if (CheckSphere == false)
         {
-            aidepose = new Vector2(aide.transform.position.x, aide.transform.position.y);
-            CheckSphere = Physics2D.OverlapCircle(aidepose, 0.1f);
-            if (CheckSphere == false)
-            {
-                is_crouching = false;
-                moveSpeed_horizontal = 400f;
-                cap.offset = new Vector2(0f, -0.35f);
-                cap.size = new Vector2(1f, 1.3f);
-                cap.direction = CapsuleDirection2D.Vertical;
-                animController.SetBool("Crouching", false);
-            }
+            is_crouching = false;
+            moveSpeed_horizontal = 400f;
+            cap.offset = new Vector2(0f, -0.35f);
+            cap.size = new Vector2(1f, 1.3f);
+            cap.direction = CapsuleDirection2D.Vertical;
+            animController.SetBool("Crouching", false);
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         grounded = true;
         animController.SetBool("Jumping", false);
+
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            canClimb = true;
+            grounded = false;
+            Debug.Log("enter");
+        }       
+        
     }
-    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            canClimb = false;
+            grounded = true;
+            rb.gravityScale = 3f;
+            Debug.Log("exit");
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {   
-        animController.SetBool("Jumping", false);        
+        animController.SetBool("Jumping", false);    
     }
+
 
 }
